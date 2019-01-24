8326186cc92b053fc531202f06009baf9dbe719c
TangQiuHang <mcorleon95@outlook.com>
2019-01-24 13:50:28 +0800

    update timeline

--- file/functions.js
+++ file/functions.js
@@ -30,6 +30,7 @@ $(window).resize(function () {
                 $ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
                 if (progress >= str.length) {
                     clearInterval(timer);
+                    $('#clock-box').fadeIn(4000);
                 }
             }, 75);
         });
@@ -39,26 +40,21 @@ $(window).resize(function () {
 
 function timeElapse(date) {
 
-    // }
     var date1 = date;  //开始时间
 
     var date2 = new Date();    //结束时间
     var date3 = date2.getTime() - date1.getTime()  //时间差的毫秒数
-
-
-//计算出相差天数
+    //计算出相差天数
     var days = Math.floor(date3 / (24 * 3600 * 1000))
-
-//计算出小时数
-
+    //计算出小时数
     var leave1 = date3 % (24 * 3600 * 1000)    //计算天数后剩余的毫秒数
     var hours = Math.floor(leave1 / (3600 * 1000))
-//计算相差分钟数
+    //计算相差分钟数
     var leave2 = leave1 % (3600 * 1000)        //计算小时数后剩余的毫秒数
     var minutes = Math.floor(leave2 / (60 * 1000))
-//计算相差秒数
+    //计算相差秒数
     var leave3 = leave2 % (60 * 1000)      //计算分钟数后剩余的毫秒数
-    var seconds = Math.round(leave3 / 1000)
+    var seconds = Math.floor(leave3 / 1000)
     var result = " <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒";
     $("#clock").html(result);
 }
